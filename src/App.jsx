f5268b605d2c3a164b1de148f33ef3f4b8123537
linimary <mariya.lichanova@gmail.com>
2023-04-16 23:33:18 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+
+import Header from "./components/Header";
+import FeedbackList from "./components/FeedbackList";
+import FeedbackData from "./data/FeedbackData";
+
+function App() {
+  const [feedback, setFeedback] = useState(FeedbackData);
+
+  return (
+    <>
+      <Header text="Feedback UI" />
+      <div className="container">
+        <FeedbackList feedback={feedback} />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+export default function Header({ text }) {
+  return (
+    <header>
+      <div className="container">
+        <h2>{text}</h2>
+      </div>
+    </header>
+  );
+}
+
